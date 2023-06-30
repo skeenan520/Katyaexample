e7d541dd683eb43ab19beeced9f22cc0b470e911
Sean Keenan <mkeenan@brynmawr.edu>
2023-06-30 14:49:38 -0400

    Update 1

--- leafletmap.js
+++ leafletmap.js
@@ -6,3 +6,8 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
+var polygon = L.polygon([
+  [51.509, -0.08],
+  [51.503, -0.06],
+  [51.51, -0.047]
+]).addTo(map);
